da95e18ed0f52c82e2a107b0cb136f54b5b2d257
Dilbert66 <aturbide99@gmail.com>
2024-11-07 13:16:26 -0500

    - mg lib cosmetic fixes

--- components/mg_lib/__init__.py
+++ components/mg_lib/__init__.py
@@ -4,12 +4,13 @@ import pathlib
 from esphome.helpers import copy_file_if_changed
 from esphome.core import CORE
 
-
 CONFIG_SCHEMA = cv.Schema({})
-
 
 async def to_code(config):
     copy_file_if_changed(
        os.path.join(pathlib.Path(__file__).parent.resolve(),"mongoose.h"),
        CORE.relative_build_path("src/mongoose.h"),
     )
+   
+
+
