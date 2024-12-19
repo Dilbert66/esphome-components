a2d2ef56a7d103d7cefa1596a578f2374629fa9f
Dilbert66 <aturbide99@gmail.com>
2024-12-18 19:56:47 -0500

    - fix mongoose lib

--- components/mg_lib/__init__.py
+++ components/mg_lib/__init__.py
@@ -1,23 +1,10 @@
-import esphome.config_validation as cv
-import os
-import pathlib
-from esphome.helpers import copy_file_if_changed
-from esphome.core import CORE,coroutine_with_priority
-import logging
-_LOGGER = logging.getLogger(__name__)
-CONFIG_SCHEMA = cv.Schema({})
+#mongoose web server library
+import esphome.codegen as cg
 
-@coroutine_with_priority(40.0)
+
 async def to_code(config):
-    src=os.path.join(pathlib.Path(__file__).parent.resolve(),"mongoose.h_h")
-    dst=CORE.relative_build_path("src/mongoose.h")
-    if os.path.isfile(src) and not os.path.isfile(dst):
-      copy_file_if_changed(src,dst)
-      _LOGGER.info("Copied mongoose.h")
-    src=os.path.join(pathlib.Path(__file__).parent.resolve(),"mongoose.c_c")
-    dst=CORE.relative_build_path("src/mongoose.c")
-    if os.path.isfile(src) and not os.path.isfile(dst):
-      copy_file_if_changed(src,dst)
-      _LOGGER.info("Copied mongoose.c")
-
+    cg.add_build_flag("-DMG_TLS=MG_TLS_MBED")
+    #cg.add_build_flag("-DMG_TLS=MG_TLS_BUILTIN")
+    cg.add_build_flag("-DMG_IO_SIZE=512")
+    # cg.add_build_flag("-DMG_ENABLE_POLL")
 
