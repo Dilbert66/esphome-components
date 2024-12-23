1fadb4917851449a92f9201e41a805aa68c97242
Dilbert66 <aturbide99@gmail.com>
2024-12-23 07:52:34 -0500

    - added config validation entry to avoid compilation error

--- components/mg_lib/__init__.py
+++ components/mg_lib/__init__.py
@@ -1,6 +1,13 @@
 #mongoose web server library
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+        }
+    )
+)
 
 async def to_code(config):
     cg.add_build_flag("-DMG_ARCH=MG_ARCH_ESP32")
