dbb6d13c6e709522bd0a21d03ae020b60d4471cd
Dilbert66 <aturbide99@gmail.com>
2025-09-04 07:05:20 -0400

    - update incorrect text sensor init

--- components/dsc_alarm_panel/text_sensor/__init__.py
+++ components/dsc_alarm_panel/text_sensor/__init__.py
@@ -1,18 +1,19 @@
 import esphome.config_validation as cv
-from esphome.components import binary_sensor
+from esphome.components import text_sensor
 
-from .. import  generate_validate_sensor_config, setup_alarm_sensor
+from .. import generate_validate_sensor_config, setup_alarm_sensor
 from .. import ALARM_SENSOR_SCHEMA, WEBKEYPAD_SORTING_SCHEMA
 
 CONFIG_SCHEMA = cv.All(
-    binary_sensor.binary_sensor_schema()
+    text_sensor.text_sensor_schema()
     .extend(ALARM_SENSOR_SCHEMA)
     .extend(cv.COMPONENT_SCHEMA)
     .extend(WEBKEYPAD_SORTING_SCHEMA),
-    generate_validate_sensor_config(True) # True indicates this is a binary sensor
+    generate_validate_sensor_config(False)  # False indicates this is not a binary sensor, but a text sensor
 )
 
+
 async def to_code(config):
-    var = await binary_sensor.new_binary_sensor(config)
-    await setup_alarm_sensor(var,config,True)
+    var = await text_sensor.new_text_sensor(config)
+    await setup_alarm_sensor(var,config,False)
 
