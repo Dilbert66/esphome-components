9ecec42a24408450dbd22f9f993fe39019097d96
Dilbert66 <aturbide99@gmail.com>
2025-10-16 11:44:12 -0400

    - add rf serial emulation setup code

--- components/vista_alarm_panel/__init__.py
+++ components/vista_alarm_panel/__init__.py
@@ -58,8 +58,12 @@ CONF_SORTING_GROUP_ID = "sorting_group_id"
 CONF_SORTING_WEIGHT = "sorting_weight"
 CONF_WEB_KEYPAD_ID="web_keypad_id"
 CONF_WEB_KEYPAD="web_keypad"
-CONF_RF_SERIAL="rf_serial"
-CONF_RF_LOOP="rf_loop"
+CONF_DEVICE_SERIAL="device_serial"
+CONF_DEVICE_LOOP="device_loop"
+CONF_DEVICE_TYPE="device_type"
+CONF_RF_ADDR="rf_receiver_addr"
+CONF_EMULATE_RF_RECEIVER="emulate_rf_receiver"
+CONF_EMULATED="emulated"
 
 BINARY_SENSOR_TYPE_ID_REGEX = r"^(ac|bat|trbl_\d+|byp_\d+|rdy_\d+|arm_\d+|arma_\d+|arms_\d+|armi_\d+|armn_\d+|alm_\d+|fire_\d+|chm_\d+|r\d+|z\d+)$"
 BINARY_SENSOR_TYPE_ID_DESCRIPTION = "ac, bat, trbl_<digits>,byp_<digits>, rdy_<digits>, arm_<digits>, arma_<digits>, arms_<digits>, arma_<digits>, armn_<digits>,alm_<digits>, fire_<digits>, chm_<digits>, r<digits>, z<digits>"
@@ -107,6 +111,8 @@ CONFIG_SCHEMA = cv.Schema(
     cv.Optional(CONF_INPUT_RX,default='INPUT'): cv.one_of('INPUT_PULLUP','INPUT_PULLDOWN','INPUT',upper=True),
     cv.Optional(CONF_INPUT_MON,default='INPUT'): cv.one_of('INPUT_PULLUP','INPUT_PULLDOWN','INPUT',upper=True),
     cv.Optional(CONF_STACK_SIZE):cv.int_,
+    cv.Optional(CONF_EMULATE_RF_RECEIVER,default= 'false'): cv.boolean,
+    cv.Optional(CONF_RF_ADDR,default=0xff): cv.int_, 
     }
 )
 
@@ -149,11 +155,13 @@ WEBKEYPAD_SORTING_SCHEMA = cv.Schema(
 
 ALARM_SENSOR_SCHEMA = cv.Schema(
     {
+        cv.GenerateID(CONF_ALARM_ID): cv.use_id(AlarmComponent),
         cv.Optional(CONF_TYPE_ID, default=""): cv.Any(cv.string_strict, validate_id_code),
         cv.Optional(CONF_PARTITION,default=0): cv.int_,
-        cv.GenerateID(CONF_ALARM_ID): cv.use_id(AlarmComponent),
-        cv.Optional(CONF_RF_SERIAL,default=0):cv.int_,
-        cv.Optional(CONF_RF_LOOP,default=0):cv.int_,
+        cv.Optional(CONF_DEVICE_SERIAL,default=0):cv.int_,
+        cv.Optional(CONF_DEVICE_LOOP,default=0):cv.int_,
+        cv.Optional(CONF_DEVICE_TYPE,default="WIRED"): cv.one_of("WIRED","RF","LOOP",upper=True),
+        cv.Optional(CONF_EMULATED,default=False):cv.boolean
     }
 )
 
@@ -172,9 +180,9 @@ async def to_code(config):
     if CORE.is_esp8266:
         cg.add_define("INPUT_PULLDOWN INPUT")
     old_dir = CORE.relative_build_path("src")    
-#    if config[CONF_CLEAN] or os.path.exists(old_dir+'/vistaalarm.h'):
-#        real_clean_build()
-    
+    # if config[CONF_CLEAN] or os.path.exists(old_dir / 'vistaalarm.h'):
+    #     real_clean_build()
+
     if  config[CONF_USEASYNC]:
         cg.add_define("USETASK")
     var = cg.new_Pvariable(config[CONF_ID],config[CONF_KEYPAD1],config[CONF_RXPIN],config[CONF_TXPIN],config[CONF_MONITORPIN],config[CONF_MAXZONES],config[CONF_MAXPARTITIONS],config[CONF_INVERT_RX],config[CONF_INVERT_TX],config[CONF_INVERT_MON],cg.RawExpression(config[CONF_INPUT_RX]),cg.RawExpression(config[CONF_INPUT_MON]))
@@ -182,74 +190,80 @@ async def to_code(config):
     if CONF_ACCESSCODE in config:
         cg.add(var.set_accessCode(config[CONF_ACCESSCODE]));
     if CONF_MAXZONES in config:
-        cg.add(var.set_maxZones(config[CONF_MAXZONES]));
+        cg.add(var.set_maxZones(config[CONF_MAXZONES]))
     if CONF_RFSERIAL in config:
-        cg.add(var.set_rfSerialLookup(config[CONF_RFSERIAL]));
+        cg.add(var.set_rfSerialLookup(config[CONF_RFSERIAL]))
     if CONF_DEFAULTPARTITION in config:
-        cg.add(var.set_defaultPartition(config[CONF_DEFAULTPARTITION]));
+        cg.add(var.set_defaultPartition(config[CONF_DEFAULTPARTITION]))
     if CONF_DEBUGLEVEL in config:
-        cg.add(var.set_debug(config[CONF_DEBUGLEVEL]));
+        cg.add(var.set_debug(config[CONF_DEBUGLEVEL]))
     if CONF_KEYPAD1 in config:
-        cg.add(var.set_partitionKeypad(1,config[CONF_KEYPAD1]));
+        cg.add(var.set_partitionKeypad(1,config[CONF_KEYPAD1]))
     if CONF_KEYPAD2 in config:
-        cg.add(var.set_partitionKeypad(2,config[CONF_KEYPAD2]));
+        cg.add(var.set_partitionKeypad(2,config[CONF_KEYPAD2]))
     if CONF_KEYPAD3 in config:
-        cg.add(var.set_partitionKeypad(3,config[CONF_KEYPAD3]));
+        cg.add(var.set_partitionKeypad(3,config[CONF_KEYPAD3]))
     if CONF_EXPANDER1 in config:
-        cg.add(var.set_expanderAddr(config[CONF_EXPANDER1]));
+        cg.add(var.set_expanderAddr(config[CONF_EXPANDER1]))
     if CONF_EXPANDER2 in config:
-        cg.add(var.set_expanderAddr(config[CONF_EXPANDER2]));
+        cg.add(var.set_expanderAddr(config[CONF_EXPANDER2]))
     if CONF_RELAY1 in config:
-        cg.add(var.set_expanderAddr(config[CONF_RELAY1]));
+        cg.add(var.set_expanderAddr(config[CONF_RELAY1]))
     if CONF_RELAY2 in config:
-        cg.add(var.set_expanderAddr(config[CONF_RELAY2]));
+        cg.add(var.set_expanderAddr(config[CONF_RELAY2]))
     if CONF_RELAY3 in config:
-        cg.add(var.set_expanderAddr(config[CONF_RELAY3]));
+        cg.add(var.set_expanderAddr(config[CONF_RELAY3]))
     if CONF_RELAY4 in config:
-        cg.add(var.set_expanderAddr(config[CONF_RELAY4]));
+        cg.add(var.set_expanderAddr(config[CONF_RELAY4]))
     if CONF_TTL in config:
-        cg.add(var.set_ttl(config[CONF_TTL]));        
+        cg.add(var.set_ttl(config[CONF_TTL]))        
     if CONF_QUICKARM in config:
-        cg.add(var.set_quickArm(config[CONF_QUICKARM]));        
+        cg.add(var.set_quickArm(config[CONF_QUICKARM]))       
     if CONF_LRR in config:
-        cg.add(var.set_lrrSupervisor(config[CONF_LRR]));      
+        cg.add(var.set_lrrSupervisor(config[CONF_LRR]))     
     if CONF_AUIADDR in config:
-        cg.add(var.set_auiaddr(config[CONF_AUIADDR]));
+        cg.add(var.set_auiaddr(config[CONF_AUIADDR]))
     if CONF_FAULT in config:
-        cg.add(var.set_text(1,config[CONF_FAULT])); 
+        cg.add(var.set_text(1,config[CONF_FAULT]))
     if CONF_BYPASS in config:
-        cg.add(var.set_text(2,config[CONF_BYPASS])); 
+        cg.add(var.set_text(2,config[CONF_BYPASS]))
     if CONF_ALARM in config:
-        cg.add(var.set_text(3,config[CONF_ALARM])); 
+        cg.add(var.set_text(3,config[CONF_ALARM]))
     if CONF_FIRE in config:
-        cg.add(var.set_text(4,config[CONF_FIRE])); 
+        cg.add(var.set_text(4,config[CONF_FIRE]))
     if CONF_CHECK in config:
-        cg.add(var.set_text(5,config[CONF_CHECK])); 
+        cg.add(var.set_text(5,config[CONF_CHECK]))
     if CONF_TRBL in config:
-        cg.add(var.set_text(6,config[CONF_TRBL]));  
+        cg.add(var.set_text(6,config[CONF_TRBL]))  
     if CONF_HITSTAR in config:
-        cg.add(var.set_text(7,config[CONF_HITSTAR]));  
-
+        cg.add(var.set_text(7,config[CONF_HITSTAR])) 
+    if config[CONF_EMULATE_RF_RECEIVER]:
+        cg.add(var.set_rf_emulation(config[CONF_EMULATE_RF_RECEIVER]))
+    cg.add(var.set_rf_addr(config[CONF_RF_ADDR]))
   
     await cg.register_component(var, config)
  
-#def real_clean_build():
-#    import shutil
-#    build_dir = CORE.relative_build_path("")
-#    if os.path.isdir(build_dir):
-#        _LOGGER.info("Deleting %s", build_dir)
-#        shutil.rmtree(build_dir)
+# def real_clean_build():
+#     import shutil
+#     build_dir = CORE.relative_build_path("")
+#     if os.path.isdir(build_dir):
+#         _LOGGER.info("Deleting %s", build_dir)
+#         shutil.rmtree(build_dir)
 
 async def setup_alarm_sensor(var, config,is_binary_sensor=True):
     """Set up custom properties for an alarm sensor"""
     paren = await cg.get_variable(config[CONF_ALARM_ID])
-
+    device_type=0
+    if config[CONF_DEVICE_TYPE] =="RF":
+        device_type=1
+    if config[CONF_DEVICE_TYPE] =="LOOP":
+        device_type=2
     if config.get(CONF_TYPE_ID):
         cg.add(var.set_object_id(sanitize(snake_case(config[CONF_TYPE_ID]))))
-        cg.add(paren.createZoneFromObj(var,config[CONF_PARTITION],config[CONF_RF_SERIAL],config[CONF_RF_LOOP]))
+        cg.add(paren.createZoneFromObj(var,config[CONF_PARTITION],config[CONF_DEVICE_SERIAL],config[CONF_DEVICE_LOOP],device_type,config[CONF_EMULATED]))
     elif config[CONF_ID] and config[CONF_ID].is_manual:
         cg.add(var.set_object_id(sanitize(snake_case(config[CONF_ID].id))))
-        cg.add(paren.createZoneFromObj(var,config[CONF_PARTITION],config[CONF_RF_SERIAL],config[CONF_RF_LOOP]))
+        cg.add(paren.createZoneFromObj(var,config[CONF_PARTITION],config[CONF_DEVICE_SERIAL],config[CONF_DEVICE_LOOP],device_type,config[CONF_EMULATED]))
     if is_binary_sensor:
         cg.add(var.publish_state(False))
         cg.add(var.set_trigger_on_initial_state(True))
@@ -263,9 +277,15 @@ async def setup_alarm_sensor(var, config,is_binary_sensor=True):
 def generate_validate_sensor_config(is_binary_sensor=True):
     """Generate a validation function for sensor configuration."""
     def validate_sensor_config(config):
+        if config[CONF_DEVICE_SERIAL] > 0:
+            if (config[CONF_DEVICE_TYPE]=="WIRED"):
+                raise cv.Invalid(f"Option 'device_type' required for RF/LOOP zones. Allowed values: RF, LOOP")
+            if config[CONF_DEVICE_LOOP]==0 or config[CONF_DEVICE_LOOP]>3:
+                raise cv.Invalid(f"Option 'device_loop' required for RF/LOOP zones. Allowed values: 1,2 or 3")
         id_val = config.get(CONF_TYPE_ID) or config[CONF_ID].id
         validate_id_code(id_val, is_binary_sensor)
         return config
+
 
     return validate_sensor_config            
     
