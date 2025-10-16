682f4242a6b394124de90fc4ae76a083dde980af
Dilbert66 <aturbide99@gmail.com>
2025-10-16 11:45:20 -0400

    - update init

--- components/vista_alarm_panel/__init__.py
+++ components/vista_alarm_panel/__init__.py
@@ -180,8 +180,8 @@ async def to_code(config):
     if CORE.is_esp8266:
         cg.add_define("INPUT_PULLDOWN INPUT")
     old_dir = CORE.relative_build_path("src")    
-#    if config[CONF_CLEAN] or os.path.exists(old_dir+'/vistaalarm.h'):
-#        real_clean_build()
+    # if config[CONF_CLEAN] or os.path.exists(old_dir / 'vistaalarm.h'):
+    #     real_clean_build()
 
     if  config[CONF_USEASYNC]:
         cg.add_define("USETASK")
@@ -243,12 +243,12 @@ async def to_code(config):
   
     await cg.register_component(var, config)
  
-def real_clean_build():
-    import shutil
-    build_dir = CORE.relative_build_path("")
-    if os.path.isdir(build_dir):
-        _LOGGER.info("Deleting %s", build_dir)
-        shutil.rmtree(build_dir)
+# def real_clean_build():
+#     import shutil
+#     build_dir = CORE.relative_build_path("")
+#     if os.path.isdir(build_dir):
+#         _LOGGER.info("Deleting %s", build_dir)
+#         shutil.rmtree(build_dir)
 
 async def setup_alarm_sensor(var, config,is_binary_sensor=True):
     """Set up custom properties for an alarm sensor"""
