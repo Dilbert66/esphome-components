47792160bdd3258cd735d5fd46f7c8e700efd81d
Dilbert66 <aturbide99@gmail.com>
2025-12-12 17:26:40 -0500

    - added support for esp8266 in mg_lib

--- components/mg_lib/__init__.py
+++ components/mg_lib/__init__.py
@@ -1,7 +1,7 @@
 #mongoose web server library
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.core import CORE
+from esphome.core import CORE,PLATFORM_ESP32
 
 
 CONFIG_SCHEMA = cv.All(
@@ -10,23 +10,17 @@ CONFIG_SCHEMA = cv.All(
         }
     )
 )
+cv.only_on([PLATFORM_ESP32]),
 
 async def to_code(config):
     if CORE.is_esp8266:  
         cg.add_build_flag("-DMG_IO_SIZE=512")
         cg.add_build_flag("-DMG_ENABLE_CUSTOM_MILLIS=1")  
         cg.add_build_flag("-DMG_ARCH=MG_ARCH_ESP8266")
-        cg.add_build_flag("-DLWIP_TIMEVAL_PRIVATE=0")
-        #cg.add_build_flag("-DMG_ENABLE_LWIP=1")
-        #cg.add_build_flag("-DLWIP_SOCKET=1")
-        #cg.add_build_flag("-DLWIP_POSIX_SOCKETS_IO_NAMES=1")
-        #cg.add_build_flag("-DLWIP_COMPAT_SOCKETS=1")
-        #cg.add_build_flag("-DMG_TLS=MG_TLS_MBED")
+        cg.add_library("ESP32Async/ESPAsyncTCP", "2.0.0")
     else:
         cg.add_build_flag("-DMG_ARCH=MG_ARCH_ESP32")
         cg.add_build_flag("-DMG_TLS=MG_TLS_MBED")
-        #cg.add_build_flag("-DMG_ENABLE_CUSTOM_MILLIS=1")  
-        #cg.add_build_flag("-DMG_TLS=MG_TLS_BUILTIN")
         cg.add_build_flag("-DMG_IO_SIZE=512")
 
 
